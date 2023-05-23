4347e31023c3f8345deecb2cad8aa950ded5e2ee
GilHope <ghopecs@gmail.com>
2023-05-23 11:23:00 -0400

    DNS resource added

--- resume3.0/js-counter/index.js
+++ resume3.0/js-counter/index.js
@@ -2,7 +2,8 @@
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter()  {
-    let response = await fetch("https://d5uvb4hkmt25ddtlw5o4zccrk40opdiw.lambda-url.us-east-1.on.aws/");
+    // let response = await fetch("https://d5uvb4hkmt25ddtlw5o4zccrk40opdiw.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/VisitorCount");
     let data = await response.json();
     counter.innerHTML = ` Total Visitors: ${data}`;
 }
