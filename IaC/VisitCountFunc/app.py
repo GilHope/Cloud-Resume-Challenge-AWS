7b701f7ed2ef2944c29ecee5a7ffa95bd939dd25
GilHope <ghopecs@gmail.com>
2023-05-30 15:18:07 -0400

    added additonal test

--- IaC/VisitCountFunc/app.py
+++ IaC/VisitCountFunc/app.py
@@ -1,3 +1,22 @@
+# import json
+# import boto3
+# dynamodb = boto3.resource('dynamodb')
+# table = dynamodb.Table('DynaDBTableCloudResume')
+# def lambda_handler(event, context):
+#     response = table.get_item(Key={
+#         'id': '0'
+#     })
+#     view_count = response['Item']['view_count']
+#     view_count = view_count + 1
+#     print(view_count)
+#     response = table.put_item(Item={
+#         'id' : '0',
+#         'view_count':view_count
+#     })
+    
+#     return view_count
+
+######################################################################
 import json
 import boto3
 dynamodb = boto3.resource('dynamodb')
@@ -6,12 +25,18 @@ def lambda_handler(event, context):
     response = table.get_item(Key={
         'id': '0'
     })
-    view_count = response['Item']['view_count']
-    view_count = view_count + 1
+
+    if 'Item' in response:
+        view_count = response['Item'].get('view_count', 0)
+    else:
+        view_count = 0  # Or 1, depending on what you want the initial count to be
+
+    view_count += 1
     print(view_count)
+
     response = table.put_item(Item={
         'id' : '0',
-        'view_count':view_count
+        'view_count': view_count
     })
-    
+
     return view_count
--- IaC/tests/unit/test_handler.py
+++ IaC/tests/unit/test_handler.py
@@ -1,44 +1,27 @@
 
-# from VisitCountFunc import app# 
-# # Pytest Unit Test:
-# import json
-# import boto3
-# import pytest
-# from moto import mock_dynamodb
+# import unittest
+# from unittest.mock import patch, MagicMock
+# from VisitCountFunc import app  # replace 'VisitCountFunc' with the name of your Python file
 
+# class TestLambdaHandler(unittest.TestCase):
+#     @patch('VisitCountFunc.app.table')
+#     def test_lambda_handler(self, table_mock):
+#         # Set up the mock object
+#         get_item_response_mock = {'Item': {'id': '0', 'view_count': 1}}
+#         table_mock.get_item.return_value = get_item_response_mock
 
-# # The 'mock_dynamodb2' decorator is used to mimic DynamoDB for testing
-# @mock_dynamodb
-# def test_lambda_handler():
-#     # DynamoDB resource is initialized
-#     dynamodb = boto3.resource('dynamodb')
-#     # Table name is stored in a variable
-#     table_name = 'DynaDBTableCloudResume'
-#     # Table is created with the necessary schema and throughput specifications
-#     dynamodb.create_table(
-#         TableName=table_name,
-#         KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
-#         AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'N'}],
-#         ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10}
-#     )
-#     # The created table is fetched
-#     table = dynamodb.Table(table_name)
-#     # Initial view count is stored in a variable
-#     initial_count = 0
-#     # A record is added to the table with the initial view count
-#     table.put_item(Item={'id' : 0, 'view_count': initial_count})
+#         # Run the function
+#         result = app.lambda_handler({}, {})
 
-#     # The Lambda function is called, and we assert that it returns the incremented view count
-#     new_view_count = app.lambda_handler(None, None)
-#     assert new_view_count == initial_count + 1
+#         # Assert the results
+#         self.assertEqual(result, 2)
+#         table_mock.get_item.assert_called_once_with(Key={'id': '0'})
+#         table_mock.put_item.assert_called_once_with(Item={'id': '0', 'view_count': 2})
 
-#     # The record is fetched from the table again
-#     response = table.get_item(Key={'id': 0})
-#     # The stored view count is fetched from the response
-#     stored_count = response['Item']['view_count']
-#     # We assert that the stored view count is the incremented view count
-#     assert stored_count == initial_count + 1
+# if __name__ == '__main__':
+#     unittest.main()
 
+#####################################################################################
 
 import unittest
 from unittest.mock import patch, MagicMock
@@ -48,16 +31,31 @@ class TestLambdaHandler(unittest.TestCase):
     @patch('VisitCountFunc.app.table')
     def test_lambda_handler(self, table_mock):
         # Set up the mock object
-        get_item_response_mock = {'Item': {'id': '0', 'view_count': 1}}
+        initial_view_count = 1
+        get_item_response_mock = {'Item': {'id': '0', 'view_count': initial_view_count}}
         table_mock.get_item.return_value = get_item_response_mock
 
         # Run the function
         result = app.lambda_handler({}, {})
 
         # Assert the results
-        self.assertEqual(result, 2)
+        self.assertEqual(result, initial_view_count + 1)
         table_mock.get_item.assert_called_once_with(Key={'id': '0'})
-        table_mock.put_item.assert_called_once_with(Item={'id': '0', 'view_count': 2})
+        table_mock.put_item.assert_called_once_with(Item={'id': '0', 'view_count': initial_view_count + 1})
+
+    @patch('VisitCountFunc.app.table')
+    def test_lambda_handler_no_initial_item(self, table_mock):
+        # Set up the mock object with no initial item
+        get_item_response_mock = {}
+        table_mock.get_item.return_value = get_item_response_mock
+
+        # Run the function
+        result = app.lambda_handler({}, {})
+
+        # Assert the results
+        self.assertEqual(result, 1)  # Assumes function initializes count to 1 if no initial item
+        table_mock.get_item.assert_called_once_with(Key={'id': '0'})
+        table_mock.put_item.assert_called_once_with(Item={'id': '0', 'view_count': 1})
 
 if __name__ == '__main__':
     unittest.main()
