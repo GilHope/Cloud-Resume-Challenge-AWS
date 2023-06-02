7532ec600f83308103f20e6c5efc6e222c3da478
GilHope <ghopecs@gmail.com>
2023-06-02 10:13:20 -0400

    cors update

--- IaC/VisitCountFunc/app.py
+++ IaC/VisitCountFunc/app.py
@@ -71,6 +71,9 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": { "Content-Type": "text/plain" },
+        "headers": { 
+            "Content-Type": "text/plain",
+            "Access-Control-Allow-Origin" : "*",
+        },
         "body": json.dumps(view_count)
     }
