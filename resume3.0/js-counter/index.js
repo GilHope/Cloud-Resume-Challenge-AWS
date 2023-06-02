a920bc07132ad6b6aabfe30a7086bbd1eb9e39c1
GilHope <ghopecs@gmail.com>
2023-06-02 12:49:53 -0400

    js update

--- resume3.0/js-counter/index.js
+++ resume3.0/js-counter/index.js
@@ -1,14 +1,26 @@
 // GH-CLOUD-RESUME VISITOR COUNTER
 
-const counter = document.querySelector(".counter-number");
-async function updateCounter()  {
-    // let response = await fetch("https://d5uvb4hkmt25ddtlw5o4zccrk40opdiw.lambda-url.us-east-1.on.aws/");
-    // let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/VisitorCount");
-    let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount");
-    let data = await response.json();
-    // console.log(data);  // log the response data
-    console.log('Total visitors:', data.body);
-    counter.innerHTML = ` Total Visitors: ${data.body}`;
-}
+// const counter = document.querySelector(".counter-number");
+// async function updateCounter()  {
+//     // let response = await fetch("https://d5uvb4hkmt25ddtlw5o4zccrk40opdiw.lambda-url.us-east-1.on.aws/");
+//     // let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/VisitorCount");
+//     let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount");
+//     let data = await response.json();
+//     // console.log(data);  // log the response data
+//     console.log('Total visitors:', data.body);
+//     counter.innerHTML = ` Total Visitors: ${data.body}`;
+// }
 
-updateCounter();
+// updateCounter(); 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const counter = document.querySelector(".counter-number");
+    async function updateCounter()  {
+        let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount");
+        let data = await response.json();
+        console.log('Total visitors:', data.body);  // Log the number of views
+        counter.innerHTML = ` Total Visitors: ${data.body}`;  // Set the HTML content
+    }
+
+    updateCounter();
+});
