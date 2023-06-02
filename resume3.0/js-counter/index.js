dd15f57479d5ea8c84ac6aa9a47eef95cab21b32
GilHope <ghopecs@gmail.com>
2023-06-02 12:22:10 -0400

    js update

--- resume3.0/js-counter/index.js
+++ resume3.0/js-counter/index.js
@@ -6,9 +6,9 @@ async function updateCounter()  {
     // let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/VisitorCount");
     let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount");
     let data = await response.json();
-    console.log(data);  // log the response data
+    // console.log(data);  // log the response data
     console.log('Total visitors:', data.body);
-    counter.innerHTML = ` Total Visitors: ${data.body}`;
+    // counter.innerHTML = ` Total Visitors: ${data.body}`;
 }
 
 updateCounter();
