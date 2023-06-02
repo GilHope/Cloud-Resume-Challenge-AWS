69a10e097393020a6354063b081e188f781cf541
GilHope <ghopecs@gmail.com>
2023-06-02 14:31:41 -0400

    js update

--- resume3.0/js-counter/index.js
+++ resume3.0/js-counter/index.js
@@ -13,17 +13,17 @@
 
 // updateCounter(); 
 
-const counter = document.querySelector(".counter-number");
-async function updateCounter()  {
-    // let response = await fetch("https://d5uvb4hkmt25ddtlw5o4zccrk40opdiw.lambda-url.us-east-1.on.aws/");
-    // let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/VisitorCount");
-    let response = await fetch("https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount");
+window.addEventListener('load', async (event) => {
+    const response = await fetch('https://25h2mgx3kj.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount');
     let data = await response.json();
-    console.log('API response data:', data);
-    // console.log(data);  // log the response data
-    console.log(response);
-    console.log('Total visitors:', data.body);
-    counter.innerHTML = ` Total Visitors: ${data.body}`;
-}
 
-updateCounter(); 
+    console.log('API response data:', data); // log full response
+
+    let visitorCount = data; // use data directly as it's a number
+    let counterElement = document.querySelector('.counter-number');
+
+    if (counterElement) {
+        counterElement.textContent = visitorCount;
+    }
+});
+
