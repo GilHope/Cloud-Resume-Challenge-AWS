7e7d3957e114a220576af076652acfab83aff558
GilHope <ghopecs@gmail.com>
2023-06-04 00:26:00 -0400

    javascript api changes

--- resume3.0/js-counter/index.js
+++ resume3.0/js-counter/index.js
@@ -28,42 +28,20 @@
 // });
 ////////////////////////////////////////////////////////////////////
 
-// window.addEventListener('load', async (event) => {
-//     const response = await fetch('https://gpk55xge05.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount');
-//     let data = await response.json();
-
-//     console.log('API response data:', data); // log full response
-
-//     let visitorCount = "Total Visitors: " + data.view_count; // extract view_count directly from data
-//     let counterElement = document.querySelector('.counter-number');
-
-//     if (counterElement) {
-//         counterElement.textContent = visitorCount;
-//     }
-// });
-//////////////////////////////////////////////////////
-
 window.addEventListener('load', async (event) => {
-    try {
-      const apiUrlResponse = await fetch('/api/getApiUrl');
-      const apiUrlData = await apiUrlResponse.json();
-      const apiUrl = apiUrlData.api_url;
-  
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-  
-      console.log('API response data:', data);
-  
-      const visitorCount = "Total Visitors: " + data.view_count;
-      const counterElement = document.querySelector('.counter-number');
-  
-      if (counterElement) {
+    const response = await fetch('https://gpk55xge05.execute-api.us-east-1.amazonaws.com/Prod/VisitorCount');
+    let data = await response.json();
+
+    console.log('API response data:', data); // log full response
+
+    let visitorCount = "Total Visitors: " + data.view_count; // extract view_count directly from data
+    let counterElement = document.querySelector('.counter-number');
+
+    if (counterElement) {
         counterElement.textContent = visitorCount;
-      }
-    } catch (error) {
-      console.error('Failed to fetch visitor count:', error);
     }
-  });
+});
+
   
   
 
